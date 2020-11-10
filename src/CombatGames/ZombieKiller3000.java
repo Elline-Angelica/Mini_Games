fed77938d5f5c210315a7e913524b485fddbd3e8
Apple <ellinemedrano1408@hotmail.com>
2020-11-10 11:47:25 +0100

    Mini Game 1.0

---
+++ src/CombatGames/ZombieKiller3000.java
@@ -0,0 +1,64 @@
+package CombatGames;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ZombieKiller3000 {
+
+    public static void main(String[] args) {
+
+
+        String backpack[] ={"Shotgun", "Assault Rifle", "Sniper"};
+        String zombies[] ={"Close-range", "Mid-range", "Long-range"};
+
+        Random rand = new Random();
+        int upperbound = 3;
+        int rzombie = rand.nextInt(upperbound);
+
+        Scanner scanner = new Scanner(System.in);
+
+        //On screen display
+        System.out.println("Welcome to our zombie killing program!");
+        System.out.println("Press any key to play");
+        String input2 = scanner.next();
+
+        if(input2 == input2){
+            System.out.println("You have a zombie approaching!");
+            System.out.println("a " + zombies[rzombie] + " zombie is approaching..");
+            System.out.println();
+            System.out.println("To kill close-range zombies you must pick a shotgun");
+            System.out.println("To kill mid-range zombies you must pick an assault rifle");
+            System.out.println("To kill long-range zombies you must pick a sniper");
+        }
+
+        System.out.println();
+        System.out.println("Are you ready? Press any key to continue");
+        String input3 = scanner.next();
+
+
+        //On screen options
+
+        if(input3 == input3) {
+
+            boolean rightChoice = true;
+
+            System.out.println("Choose your weapon!");
+            System.out.println("Press 0 for a shotgun, 1 for an assault rifle or 2 for a sniper");
+
+            int input = scanner.nextInt();
+            if(input == rzombie){
+                System.out.println("Hooray you live!");
+                System.out.println("You chose: " + backpack[input]);
+                System.out.println(" *dead zombie sounds* ");
+            } else {
+                System.out.println("Wrong choice! Game over.");
+                System.out.println("You chose: " + backpack[input] + ", in stead of " + backpack[rzombie]);
+                System.out.println("You've been eaten by the zombie! Loser..");
+            }
+        }
+
+
+
+    }
+
+}
