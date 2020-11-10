9d3eec1c9723152c293468da547d4ac052c4da26
Apple <ellinemedrano1408@hotmail.com>
2020-11-10 14:07:42 +0100

    Modified weapon indication options

--- src/CombatGames/ZombieKiller3000.java
+++ src/CombatGames/ZombieKiller3000.java
@@ -8,7 +8,7 @@ public class ZombieKiller3000 {
     public static void main(String[] args) {
 
 
-        String[] backpack ={"Shotgun", "Assault Rifle", "Sniper"};
+        String[] backpack ={"empty slot", "Shotgun", "Assault Rifle", "Sniper"};
         String[] zombies ={"Close-range", "Mid-range", "Long-range"};
 
         Random rand = new Random();
@@ -25,7 +25,7 @@ public class ZombieKiller3000 {
 
         if(input2.equals(input2)){
             System.out.println();
-            System.out.println("Look out! There's a zombie approaching!"+" An ugly ass " + zombies[rzombie] + " zombie is in sight!");
+            System.out.println("Look out! There's a zombie approaching!"+" A " + zombies[rzombie] + " zombie is in sight!");
             System.out.println("Remember...");
             System.out.println("To kill close-range zombies you must pick a shotgun");
             System.out.println("To kill mid-range zombies you must pick an assault rifle");
@@ -38,20 +38,19 @@ public class ZombieKiller3000 {
 
 
         //On screen options
-
         if(input3.equals(input3)) {
 
             System.out.println("Choose your weapon!");
-            System.out.println("Press 0 for a shotgun, 1 for an assault rifle or 2 for a sniper");
+            System.out.println("Press 1 for a shotgun, 2 for an assault rifle or 3 for a sniper");
 
             int input = scanner.nextInt();
-            if(input == rzombie){
+            if(input == (rzombie+1)){
                 System.out.println("Hooray you live!");
                 System.out.println("You chose: " + backpack[input]);
                 System.out.println(" *dead zombie sounds* ");
             } else {
                 System.out.println("Wrong choice! Game over.");
-                System.out.println("You chose: " + backpack[input] + ", in stead of " + backpack[rzombie]);
+                System.out.println("You chose: " + backpack[input] + ", in stead of " + backpack[rzombie+1]);
                 System.out.println("You've been eaten by the zombie! Loser..");
             }
         }
